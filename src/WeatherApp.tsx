9723376068c458946b06142c217416e71d1d482c
CKFranco <101433556+CKFranco@users.noreply.github.com>
2022-08-30 12:56:51 -0500

    Layout Done (#2)
    * chore: layout done
    
    * chore: icons
    
    * chore: layout done
    
    * fix: responsive
    
    Co-authored-by: rodrigo2392 <91278835+rodrigo2392@users.noreply.github.com>

--- src/WeatherApp.tsx
+++ src/WeatherApp.tsx
@@ -1,57 +1,93 @@
-import RefreshIcon from '@mui/icons-material/Refresh';
-import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
-import InvertColorsIcon from '@mui/icons-material/InvertColors';
-import SpeedIcon from '@mui/icons-material/Speed';
-import AirIcon from '@mui/icons-material/Air';
-
-
+import RefreshIcon from "@mui/icons-material/Refresh";
+import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
+import InvertColorsIcon from "@mui/icons-material/InvertColors";
+import SpeedIcon from "@mui/icons-material/Speed";
+import AirIcon from "@mui/icons-material/Air";
+import FilterDramaIcon from "@mui/icons-material/FilterDrama";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 export const WeatherApp = () => {
-
-
   return (
     <>
-     <div className="mainContainer">
-      <div className="headContainer">
-        <div className="degreesButtonContainer">
-          <span>Grados F°</span>
+      <div className="mainContainer">
+        <div className="headContainer">
+          <div className="degreesSelectContainer">
+            <div>
+              <ul>
+                <li>F°</li>
+              </ul>
+            </div>
+            <div>
+              <button className="degreesSelectButton">
+                <ArrowDropDownIcon sx={{ fontSize: 30 }} />
+              </button>
+            </div>
+          </div>
+
+          <div className="refreshButtonContainer">
+            <button>
+              <RefreshIcon className="refreshIcon" sx={{ fontSize: 30 }} />
+            </button>
+          </div>
         </div>
-        <div className="reloadButtonContainer">
-          <RefreshIcon className='icon' sx={{ fontSize: 30 }}/>
+
+        <div className="bodyContainer">
+          <div className="mainInfoContainer">
+            <div className="locationContainer">
+              <span className="locationText">Zapopan</span>
+            </div>
+            <div className="weatherIcon">
+              <FilterDramaIcon sx={{ fontSize: 70 }} />
+            </div>
+            <div className="mainInfo">
+              <span className="mainDegrees">45.79°</span>
+              <span className="weatherInfo1">Overcast Clouds</span>
+              <span className="weatherInfo2">Clouds</span>
+            </div>
+          </div>
+
+          <div className="specsInfoContainer">
+            <div className="FeelsLike">
+              <div className="iconSpecContainer">
+                <DeviceThermostatIcon className="icon" sx={{ fontSize: 30 }} />
+              </div>
+              <div className="specContainer">
+                <span className="specsTitle">Feels like:</span>
+                <span className="specsContent">43.9</span>
+              </div>
+            </div>
+
+            <div className="humidity">
+              <div className="iconSpecContainer">
+                 <InvertColorsIcon className="icon" sx={{ fontSize: 30 }} />
+              </div>
+              <div className="specContainer">
+                <span className="specsTitle">Humidity:</span>
+                <span className="specsContent">93 %</span>
+              </div>
+            </div>
+
+            <div className="windSpeed">
+              <div className="iconSpecContainer">
+                <AirIcon className="icon" sx={{ fontSize: 30 }} />
+              </div>
+              <div className="specContainer">
+                <span className="specsTitle">Wind Speed:</span>
+                <span className="specsContent">20 miles/h</span>
+              </div>
+            </div>
+
+            <div className="pressure">
+              <div className="iconSpecContainer">
+                <SpeedIcon className="icon" sx={{ fontSize: 30 }} />
+              </div>
+              <div className="specContainer">
+                <span className="specsTitle">Pressure:</span>
+                <span className="specsContent">1025 hPa</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="bodyContainer">
-
-        <div className="mainInfoContainer">
-          <span className="climaSpan">Clima:</span>
-        </div>
-
-        <div className="specsInfoContainer">
-          <div className="FeelsLike">
-            <DeviceThermostatIcon className='icon' sx={{ fontSize: 30 }} />
-            <span>Feels like:</span>
-          </div>
-          <div>
-            <InvertColorsIcon className='icon' sx={{ fontSize: 30 }}/>
-            
-            <span>Humidity:</span>
-          </div>
-          <div>
-            <AirIcon className='icon' sx={{ fontSize: 30 }}/>
-            <span>Wind Speed:</span>
-          </div>
-          <div className="windSpeed">
-            <SpeedIcon className='icon'  sx={{ fontSize: 30 }}/>
-            <span >Pressure:</span>
-          </div>
-        </div>
-
-      </div>
-     </div>
     </>
-   
-  )
-}
-
-
-
+  );
+};
