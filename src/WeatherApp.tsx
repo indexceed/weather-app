7f1e9b5b472ffb2d42775eb1a707b1ef83912382
CKFranco <101433556+CKFranco@users.noreply.github.com>
2022-09-02 14:12:57 -0500

    Develop feat: api functionality & refresh (#3)
    * chore: layout done
    
    * chore: icons
    
    * chore: layout done
    
    * fix: responsive
    
    * feat: api functionality & refresh
    
    * feat: get location added
    
    Co-authored-by: rodrigo2392 <91278835+rodrigo2392@users.noreply.github.com>

--- src/WeatherApp.tsx
+++ src/WeatherApp.tsx
@@ -1,30 +1,88 @@
+
+import axios from "axios";
+import { useState, useEffect } from "react";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
 import InvertColorsIcon from "@mui/icons-material/InvertColors";
 import SpeedIcon from "@mui/icons-material/Speed";
 import AirIcon from "@mui/icons-material/Air";
-import FilterDramaIcon from "@mui/icons-material/FilterDrama";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+
 export const WeatherApp = () => {
+
+  interface Location{
+    long: number,
+    lat: number,
+  }
+
+  const [location, setLocation] = useState<Location | null>(null);
+  const [isMetric, setIsMetric] = useState<boolean>(true);
+  const [post, setPost] = useState<any>(null);
+
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${location?.lat ? location.lat:"29.0786976"}&lon=${location?.long ? location.long:"-110.9645151"}&appid=4e91048960fd797af16731e7237484fd&units=${isMetric ? "metric" : "imperial"}&lang=es`;
+
+  const getLocation = () => { 
+     navigator.geolocation.getCurrentPosition((crd) => {
+        setLocation({
+          long: crd.coords.longitude,
+          lat: crd.coords.latitude,
+        });
+      },
+      () => {
+        setLocation({
+          long: -103.3465336,
+          lat: 20.6711442,
+        });
+      }
+     );
+  }
+  useEffect(() => {
+    if(location){
+      getData();
+    }
+    if(!location){
+      getLocation()
+    }
+  }, [isMetric,location]);
+
+  async function getData() {
+    try{
+      const result = await axios.get(baseURL);
+      setPost(result.data);
+    }
+    catch(error:any) {
+      console.log(error);
+    }
+  }
+
+  if (!post) return null;
+
+  const iconCode = post?.weather[0]?.icon;
+  const iconUrl ="http://openweathermap.org/img/wn/" + iconCode + "@4x" + ".png";
+
+  if(!location){
+    return <h1>cargando...</h1>
+  }
+  
   return (
     <>
       <div className="mainContainer">
         <div className="headContainer">
           <div className="degreesSelectContainer">
             <div>
-              <ul>
-                <li>F°</li>
-              </ul>
+              <select
+                id=""
+                onChange={(e) => setIsMetric(e.target.value === "metric")}
+              >
+                <option value="metric">C°</option>
+                <option value="imperial">F°</option>
+              </select>
             </div>
-            <div>
-              <button className="degreesSelectButton">
-                <ArrowDropDownIcon sx={{ fontSize: 30 }} />
-              </button>
-            </div>
+            <div></div>
           </div>
 
           <div className="refreshButtonContainer">
-            <button>
+            <button onClick={getData}>
               <RefreshIcon className="refreshIcon" sx={{ fontSize: 30 }} />
             </button>
           </div>
@@ -33,36 +91,38 @@ export const WeatherApp = () => {
         <div className="bodyContainer">
           <div className="mainInfoContainer">
             <div className="locationContainer">
-              <span className="locationText">Zapopan</span>
+              <span className="locationText">{post?.name}</span>
             </div>
             <div className="weatherIcon">
-              <FilterDramaIcon sx={{ fontSize: 70 }} />
+              <div id="icon">
+                <img id="wicon" src={iconUrl} alt="Weather icon" />
+              </div>
             </div>
             <div className="mainInfo">
-              <span className="mainDegrees">45.79°</span>
-              <span className="weatherInfo1">Overcast Clouds</span>
-              <span className="weatherInfo2">Clouds</span>
+              <span className="mainDegrees">{post?.main?.temp}°</span>
+              <span className="weatherInfo2">
+                {post?.weather[0]?.description.toUpperCase()}
+              </span>
             </div>
           </div>
-
           <div className="specsInfoContainer">
             <div className="FeelsLike">
               <div className="iconSpecContainer">
                 <DeviceThermostatIcon className="icon" sx={{ fontSize: 30 }} />
               </div>
               <div className="specContainer">
-                <span className="specsTitle">Feels like:</span>
-                <span className="specsContent">43.9</span>
+                <span className="specsTitle">Sensación Térmica:</span>
+                <span className="specsContent">{post?.main?.feels_like}°</span>
               </div>
             </div>
 
             <div className="humidity">
               <div className="iconSpecContainer">
-                 <InvertColorsIcon className="icon" sx={{ fontSize: 30 }} />
+                <InvertColorsIcon className="icon" sx={{ fontSize: 30 }} />
               </div>
               <div className="specContainer">
-                <span className="specsTitle">Humidity:</span>
-                <span className="specsContent">93 %</span>
+                <span className="specsTitle">Humedad:</span>
+                <span className="specsContent">{post?.main?.humidity} %</span>
               </div>
             </div>
 
@@ -71,18 +131,19 @@ export const WeatherApp = () => {
                 <AirIcon className="icon" sx={{ fontSize: 30 }} />
               </div>
               <div className="specContainer">
-                <span className="specsTitle">Wind Speed:</span>
-                <span className="specsContent">20 miles/h</span>
+                <span className="specsTitle">Viento:</span>
+                <span className="specsContent">
+                  {post?.wind?.speed} {isMetric ? "m/s" : "millas/h"}
+                </span>
               </div>
             </div>
-
             <div className="pressure">
               <div className="iconSpecContainer">
                 <SpeedIcon className="icon" sx={{ fontSize: 30 }} />
               </div>
               <div className="specContainer">
-                <span className="specsTitle">Pressure:</span>
-                <span className="specsContent">1025 hPa</span>
+                <span className="specsTitle">Presión:</span>
+                <span className="specsContent">{post?.main?.pressure} hPa</span>
               </div>
             </div>
           </div>
